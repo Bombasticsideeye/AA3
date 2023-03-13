d52f04c5b38be40f914f99eb5300bb65cc469795
Bombasticsideeye <127067588+Bombasticsideeye@users.noreply.github.com>
2023-03-13 11:04:02 +0800

    Add files via upload

---
+++ AA/loops.js
@@ -0,0 +1,15 @@
+var x = 2;
+while(x!=62) {
+    document.writeln(x );
+    x+=2;
+}
+
+var y = 3;
+while(y!=63) {
+    document.writeln(y);
+    y+=3;
+}
+
+for(var z = 5; z != 55; z+=5)  {
+    document.writeln(z);
+}
